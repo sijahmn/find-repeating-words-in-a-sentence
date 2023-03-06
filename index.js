a30c2393107b859a2b950fbe3878e1d2a24c4506
sijahmn <msijahmn@gmail.com>
2023-03-06 12:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const str = "I am Raju and Raju is my name and I am from Bangalore";
 // Split the string into an array of words - Here word are recognized based on the space in between the words.
 const words = str.split(" ");
 
-const repeated = arr.reduce((acc, item, index) => {
-  if (arr.indexOf(item) !== index) {
+const repeated = words.reduce((acc, item, index) => {
+  if (words.indexOf(item) !== index) {
     // Check if the item is already in the repeated object
     if (acc.hasOwnProperty(item)) {
       // If it is, increment its count and push the new index to its array of indexes
