f498f4afda5566762f162f335d005b0e94695698
sijahmn <msijahmn@gmail.com>
2023-03-06 12:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,21 @@ const str = "I am Raju and Raju is my name and I am from Bangalore";
 // Split the string into an array of words - Here word are recognized based on the space in between the words.
 const words = str.split(" ");
 
-// Initialize an empty object - this object works as store and help to store the repeated items, their indexes, and their count
-const repeated = {};
-
-// Loop through each word in the array and add its index to the repeated object - we can do the task either using for also.
-words.forEach((word, index) => {
-  if (words.indexOf(word) !== index) {
-    // Check if the word is already in the repeated object - here using hasOwnProperty we can find the wheather the object has the property which he has mentioned
-    if (repeated.hasOwnProperty(word)) {
-      // increment its count and push the new index to its array of indexes
-      // Here we increment value of property count and pushes index to the array indexes
-      repeated[word].count++;
-      repeated[word].indexes.push(index);
+const repeated = arr.reduce((acc, item, index) => {
+  if (arr.indexOf(item) !== index) {
+    // Check if the item is already in the repeated object
+    if (acc.hasOwnProperty(item)) {
+      // If it is, increment its count and push the new index to its array of indexes
+      acc[item].count++;
+      acc[item].indexes.push(index);
     } else {
-      // If the repeated object doesn't contain property in the mentioned name , a new property with a count of 1 and an array of indexes
-      repeated[word] = {
+      // If it's not, create a new property with a count of 1 and an array of indexes
+      acc[item] = {
         count: 1,
         indexes: [index],
       };
     }
   }
-});
-
+  return acc;
+}, {});
 console.log(repeated);
